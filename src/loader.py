5700b4aa7d3282f73ad576afc9b891b6e237a0b9
bsumanth086-collab <bsumanth086@gmail.com>
2026-03-09 00:14:09 +0530

    Delete src/loader.py

--- src/loader.py
+++
@@ -1,7 +0,0 @@
-import dask.dataframe as dd
-
-def load_parquet(path: str) -> dd.DataFrame:
-    """
-    Load large Parquet dataset using Dask.
-    """
-    return dd.read_parquet(path, engine="pyarrow")
